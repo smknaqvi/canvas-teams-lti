bf306d4a4c7e4adefeaf36ac31dbde4391d4db80
smknaqvi <k.naqvi@mail.utoronto.ca>
2021-01-08 03:42:21 -0500

    gatsby ts setup

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -1,38 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: "canvas-teams-client",
+    title: `Canvas Teams LTI`,
+    description: `A Canvas LTI app to create groups`,
+    author: ``
   },
-  plugins: [
-    "gatsby-plugin-netlify-cms",
-    "gatsby-plugin-sass",
-    "gatsby-plugin-sharp",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-offline",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
-    "gatsby-transformer-remark",
-    "gatsby-plugin-mdx",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-  ],
-};
+  plugins: [`gatsby-plugin-typescript`, `gatsby-plugin-react-helmet`]
+}

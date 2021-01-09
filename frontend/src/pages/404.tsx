0a2484bb6ce85245ea18dfe8a95107d4fddb74e1
smknaqvi <k.naqvi@mail.utoronto.ca>
2021-01-08 21:05:25 -0500

    fix bug and remove .vscode

--- frontend/src/pages/404.tsx
+++ frontend/src/pages/404.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 
-const NotFoundPage = () => (
-  <div>
-    <h1>NOT FOUND</h1>
-    <p>Uh oh... What you are looking for does not exist.</p>
-  </div>
-)
+const NotFoundPage = () => {
+  return (
+    <div>
+      <h1>NOT FOUND</h1>
+      <p>Uh oh... What you are looking for does not exist.</p>
+    </div>
+  )
+}
 
 export default NotFoundPage

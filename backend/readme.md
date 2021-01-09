4fa4176ec017daf70fa5a9885674a724bb553e89
ma-anwar <ma.rizvi.anwar@gmail.com>
2021-01-08 23:25:27 -0500

    Configure dotenv

---
+++ backend/readme.md
@@ -0,0 +1,9 @@
+### Environment Variables
+
+Configure .env with the following variables
+
+```
+PORT=
+HOST_ADDRESS=
+CANVAS_ADDRESS=
+```
--- backend/src/config/constants.ts
+++ backend/src/config/constants.ts
@@ -1,8 +1,7 @@
-// @TODO: source appropriate constants from process.env
+// @TODO: Store client ideas and secrets in database.
 export const CLIENT_ID = "10000000000004";
 export const CLIENT_SECRET =
   "SULCuWVF4E1foAy5GKm7z7hQxWUcOhLZjoBvzSdBKzN3OyNW776oaT1h69O65ozk";
-export const PORT = 3000;
-export const HOST = `http://localhost:${PORT}`;
-export const CANVAS_PORT = 8080;
-export const CANVAS = `http://localhost:${CANVAS_PORT}`;
+export const PORT = process.env.PORT;
+export const HOST_ADDRESS = process.env.HOST_ADDRESS;
+export const CANVAS_ADDRESS = process.env.CANVAS_ADDRESS;

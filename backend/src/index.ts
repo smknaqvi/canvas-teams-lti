8d264023d0a20262949bd981d5f740b3f94c3e66
Kazim <smkazimn@gmail.com>
2021-01-24 04:46:48 -0500

    add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,18 @@
 import "./config/config";
 
-import { HOST_ADDRESS, PORT } from "./config/constants";
+import cors from "cors";
 import express from "express";
+import bodyParser from "body-parser";
 import jwt from "express-jwt";
 import jwks from "jwks-rsa";
+import { HOST_ADDRESS, PORT } from "./config/constants";
 import { apiRoutes } from "./routes/api";
 const app = express();
 
 // middleware
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 const jwtCheck = jwt({
   secret: jwks.expressJwtSecret({
